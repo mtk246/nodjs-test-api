ea719b8a6b9c37dc154a92b48938f44dcab5b8df
minthukyaw <minthukyaw454@gmail.com>
2023-06-29 00:07:52 +0630

    add update api for publicStock

--- src/controllers/publicController.js
+++ src/controllers/publicController.js
@@ -31,3 +31,34 @@ exports.get = async (req, res) => {
     }
     
 };
+
+exports.update = async (req, res) {
+    const table_name = req.params.table_name;
+    const {
+        product_id,
+        product_name,
+        unit_qty,
+        packaging_type_name,
+        buy_price,
+        sell_price,
+    } = req.body;
+
+    const pool = createPool('public_db');
+
+    const rows = `UPDATE ${table_name} SET product_name = $1, unit_qty = $2, packaging_type_name = $3, buy_price = $4, sell_price = $5 WHERE product_id = $6`;
+
+    const result = await query(
+        pool,
+        rows,
+        [
+            product_name,
+            unit_qty,
+            packaging_type_name,
+            buy_price,
+            sell_price,
+            product_id
+        ]
+    );
+
+    response(res, result);
+}
